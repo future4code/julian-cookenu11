5eca99e6ec0444afa28bd357209ee0bf099859f3
Wagner Cardoso <wcardosos3421@gmail.com>
2020-07-15 15:03:42 -0300

    Novas configurações

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import { AddressInfo } from 'net';
 
+dotenv.config();
 
 const app: any = express();
 app.use(express.json());
 
-app.listen(3003);
+const server = app.listen(process.env.PORT || 3003, () => {
+    if(server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Servidor rodando em http://localhost:${address.port}...`);
+    } else {
+        console.error(`Falha na inicialização do servidor.`)
+    }
+});
