8ac1646f36a925e5b93b749b6b31b0da5f8f9a47
Wagner Cardoso <wcardosos3421@gmail.com>
2020-07-17 13:35:13 -0300

    Reconfiguração das rotas

--- src/routes.ts
+++ src/routes.ts
@@ -8,11 +8,11 @@ const routes: express.Router = express.Router();
 routes.post("/signup", UserController.store);
 routes.post("/login", UserController.login);
 routes.get("/user/profile", UserController.showProfile);
+routes.get("/user/feed", FollowersController.showFeed);
 routes.get("/user/:id", UserController.showUser);
 routes.post("/recipe", RecipeController.store);
 routes.get("/recipe/:id", RecipeController.showRecipe);
 routes.post("/user/:user_id/follow", FollowersController.store);
 routes.delete("/user/:user_id/unfollow", FollowersController.destroy);
-routes.get("/user/feed", FollowersController.showFeed);
 
 export default routes;
